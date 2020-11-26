d620af1cbd36ab803001a0bdd36c653937d26305
Matt Rasmussen <74180704+SpaceToastKADK@users.noreply.github.com>
2020-11-26 13:56:10 +0100

    Upgrade class stub

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Robot : Powered
 {
   public MonoBehaviour CPU;
-  public GameObject[] hardware;
+  public GameObject[,] upgrades = new GameObject[16, 16];
   public bool totem;
 
   // Start is called before the first frame update
-  void Start()
-  {
+  void Start(){
     setUpActualThing();
     CPU = gameObject.GetComponent<CPU>();
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update(){
+  }
+
+  public void upgrade(GameObject otherBot){
   }
 
   public override string analyze(){
---
+++ Assets/Scripts/Upgrade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : ActualThing
+{
+  public GameObject owner;
+  public float health;
+  public float maxHealth;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    setUpActualThing();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
