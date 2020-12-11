4b6bd2eda6471c20faf63b141162b0d9e51a879e
NiekMeffert <71270108+NiekMeffert@users.noreply.github.com>
2020-12-11 09:13:15 +0100

    Inventory system work

---
+++ Assets/Staging folder/Niek_Testing/Niek_Scripts/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    bool isFocus = false;
+    Transform player;
+
+    private void Update()
+    {
+        if (isFocus)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance <= radius)
+            {
+                Debug.Log("INTERACT");
+            }
+        }
+    }
+
+
+    public void onFocused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+    }
+
+    public void OnDefocused ()
+    {
+        isFocus = false;
+        player = null;
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Assets/Staging folder/Niek_Testing/Niek_Scripts/PlayerInteractable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractable : MonoBehaviour
+{
+    public Camera cam;
+    public Interactable focus;
+    public LayerMask movementmask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementmask))
+            {
+                Debug.Log("We hit" + hit.collider.name + " " + hit.point);
+              //  RemoveFocus;
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void SetFocus (Interactable newFocus)
+    {
+        focus = newFocus;
+        newFocus.onFocused(transform);
+    }
+
+    void RemoveFocus ()
+    {
+        focus.OnDefocused();
+        focus = null;
+    }
+}
