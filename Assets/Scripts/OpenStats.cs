761b7475d1f241fb88928d2cb5c13060ab1c6623
NiekMeffert <71270108+NiekMeffert@users.noreply.github.com>
2021-01-12 12:19:35 +0100

    Hide stats

---
+++ Assets/Scripts/OpenStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenStats : MonoBehaviour
+{
+    public GameObject stats;
+    // Start is called before the first frame update
+  
+    public void Openstats()
+    {
+        if(stats !=null)
+        {
+            bool isActive = stats.activeSelf;
+
+            stats.SetActive(!isActive);
+        }
+    }
+}
