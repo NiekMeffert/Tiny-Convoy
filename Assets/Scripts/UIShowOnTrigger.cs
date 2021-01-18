80641499d6ddd9a0c8062802371f642279844bd8
NiekMeffert <71270108+NiekMeffert@users.noreply.github.com>
2021-01-18 14:52:48 +0100

    blah

---
+++ Assets/Scripts/UIShowOnTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShowOnTrigger : MonoBehaviour
+{
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(uiObject);
+    }
+}
